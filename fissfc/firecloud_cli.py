a1b00e26851c90a27837f7d6d9ea46d2b6cb92d3
Timothy DeFreitas <timdef@broadinstitute.org>
2016-03-03 11:59:57 -0500

    Version bump for push to PyPI

--- fissfc/firecloud_cli.py
+++ fissfc/firecloud_cli.py
@@ -8,7 +8,7 @@ from six import print_
 from yapsy.PluginManager import PluginManager
 import os
 
-__version__="0.3.2"
+__version__="0.4.0"
 PLUGIN_PLACES = [os.path.expanduser('~/.fissfc/plugins'), "plugins"]
 
 #################################################
