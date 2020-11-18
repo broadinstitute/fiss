69eae72d7d66a58a01fe95120188a70854b7c073
Morgan Taylor <marymorg@broadinstitute.org>
2020-11-18 10:32:08 -0500

    add google cloud storage to setup.py requirements

--- setup.py
+++ setup.py
@@ -143,6 +143,7 @@ setup(
     test_suite = 'nose.collector',
     install_requires = [
         'google-auth>=1.6.3',
+        'google-cloud-storage>=1.20.0',
         'pydot',
         'requests[security]',
         'setuptools>=40.3.0',
