405a9ee93a637b3a97d06dda16c76eb13b53d6b0
David Heiman <dheiman@broadinstitute.org>
2021-01-29 14:06:18 -0500

    Suppress py2 deprecation warning

--- firecloud/__init__.py
+++ firecloud/__init__.py
@@ -1,7 +1,7 @@
 import os
 import warnings
 warnings.filterwarnings('ignore', 'Your application has authenticated using end user credentials from Google Cloud SDK. We recommend that most server applications use service accounts instead. If your application continues to use end user credentials from Cloud SDK, you might receive a "quota exceeded" or "API not enabled" error. For more information about service accounts, see https://cloud.google.com/docs/authentication/')
-
+warnings.filterwarnings('ignore', 'Python 2 is no longer supported by the Python core team. Support for it is now deprecated in cryptography, and will be removed in a future release.')
 
 # Based on https://stackoverflow.com/a/379535
 def which(program):
