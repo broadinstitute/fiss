95e03f1274a39b8abf60648b8b9429dc2ad2ee53
David Heiman <dheiman@broadinstitute.org>
2022-09-23 22:20:45 -0400

    v0.16.34 (#180)
    api.py
       * check for presence of clock skew argument rather than check google.auth.__version__
    fiss.py
       * meth_acl/config_acl
          - changed map(f(x),iterable) returns into list comprehensions to make py2/3 consistent
       * mop updated to account for change in Terra output directory structure
    fccore.py
       * removed version string parser in favor of above check for the clock skew argument

--- firecloud/fccore.py
+++ firecloud/fccore.py
@@ -21,7 +21,6 @@ import configparser
 import tempfile
 import shutil
 import subprocess
-import re
 from io import IOBase
 from firecloud import __about__
 from google.auth import environment_vars
@@ -219,47 +218,5 @@ def edit_file(name, backup=None):
     subprocess.call([__EDITOR__, name])
     current_tolm  = os.stat(name).st_mtime
     return current_tolm != previous_tolm
-
-
-# From PEP-440:
-# https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
-VERSION_PATTERN = r"""
-    v?
-    (?:
-        (?:(?P<epoch>[0-9]+)!)?                           # epoch
-        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
-        (?P<pre>                                          # pre-release
-            [-_\.]?
-            (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
-            [-_\.]?
-            (?P<pre_n>[0-9]+)?
-        )?
-        (?P<post>                                         # post release
-            (?:-(?P<post_n1>[0-9]+))
-            |
-            (?:
-                [-_\.]?
-                (?P<post_l>post|rev|r)
-                [-_\.]?
-                (?P<post_n2>[0-9]+)?
-            )
-        )?
-        (?P<dev>                                          # dev release
-            [-_\.]?
-            (?P<dev_l>dev)
-            [-_\.]?
-            (?P<dev_n>[0-9]+)?
-        )?
-    )
-    (?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
-"""
-
-version_regex = re.compile(
-    r"^\s*" + VERSION_PATTERN + r"\s*$",
-    re.VERBOSE | re.IGNORECASE,
-)
-
-def release_tuple_from_version_string(version_string):
-    return tuple(int(val) for val in version_regex.match(version_string).group('release').split('.'))
 
 # }}}
