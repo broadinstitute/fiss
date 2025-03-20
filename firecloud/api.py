b1a37188d3fd00e01f8a14a2ab73374902fb1e17
Kevin Marete <kevinmmarete@gmail.com>
2025-03-20 10:45:08 -0400

    Revert "Updated v2 storage Cost Estimate endpoint"
    This reverts commit 88df514cacb18ee26da329341789031e782f5b96.

--- firecloud/api.py
+++ firecloud/api.py
@@ -1395,9 +1395,9 @@ def get_storage_cost(namespace, workspace):
         namespace (str): project to which workspace belongs
         workspace (str): Workspace name
     Swagger:
-        https://api.firecloud.org/#/WorkspacesV2/getStorageCostEstimateV2
+        https://api.firecloud.org/#/Workspaces/getStorageCostEstimate
     """
-    uri = "workspaces/v2/{0}/{1}/storageCostEstimate".format(namespace, workspace)
+    uri = "workspaces/{0}/{1}/storageCostEstimate".format(namespace, workspace)
     return __get(uri)
 
 def get_bucket_usage(namespace, workspace):
@@ -1406,9 +1406,9 @@ def get_bucket_usage(namespace, workspace):
         namespace (str): project to which workspace belongs
         workspace (str): Workspace name
     Swagger:
-        https://api.firecloud.org/#/WorkspacesV2/getStorageCostEstimateV2
+        https://api.firecloud.org/#!/Workspaces/getBucketUsage
     """
-    uri = "workspaces/v2/{0}/{1}/storageCostEstimate".format(namespace, workspace)
+    uri = "workspaces/{0}/{1}/bucketUsage".format(namespace, workspace)
     return __get(uri)
 
 def create_workspace(namespace, name, authorizationDomain="", attributes=None,
